e9b9e2fc3d3345b45ebfac286b6e69f3c317d757
mat2718 <matthewterry68.mt@gmail.com>
2021-08-06 17:53:56 -0400

    minor updates

--- SQL Scripts/Project 3 Backend Data.sql
+++ SQL Scripts/Project 3 Backend Data.sql
@@ -1,122 +1,9 @@
 --**************************************************************************
 --**************************************************************************
--- project 3 tables
+-- project 3 Data
 --**************************************************************************
 --**************************************************************************
 
-
---//////////////////////////////////////////////////////////////////////////
--- creating regular tables
---//////////////////////////////////////////////////////////////////////////
-
-DROP TABLE skills CASCADE;
-
--- creating table skills
-CREATE Table skills (
-	skillid serial NOT null primary key,
-	skillname varchar NOT NULL);
-
-DROP TABLE curriculum CASCADE;
-
--- creating a table for curriculum
-CREATE Table curriculum (
-	curriculumid serial PRIMARY KEY,
-	createdby varchar NOT NULL,
-	createdon date NOT null default CURRENT_DATE,
-	lastmodified date NOT null default CURRENT_DATE,
-	lastmodifiedby varchar,
-	curriculumname varchar NOT null
-);
-
-DROP TABLE trainer CASCADE;
-
--- create table for trainer
-CREATE Table trainer(
-	trainerid serial not null primary key,
-	email varchar UNIQUE not null,
-	trainerfirst varchar not null,
-	trainerlast varchar not null
-);
-
-DROP TABLE client CASCADE;
-
--- creating table client
-CREATE Table client (
-	clientid serial not null primary key,
-	clientname varchar not null);
-
-DROP TABLE batch CASCADE;
-
--- creating table for batch
-CREATE Table batch ( 
-	batchid serial PRIMARY KEY NOT NULL,
-	batchsize int NOT NULL,
-	enddate varchar NOT NULL,
-	startdate varchar NOT NULL,
-	trainerid int NOT NULL,
-	clientid int NOT NULL,
-	confirmed boolean default false, 
-	curriculumid int NOT NULL,
-	constraint batch_curriculum_fk1 foreign key (curriculumid) references curriculum(curriculumid),
-	constraint batch_trainer_fk1 foreign key (trainerid) references trainer(trainerid),
-	constraint batch_client_fk1 foreign key (clientid) references client(clientid)
- );
-
-DROP TABLE clientdemand CASCADE;
-
--- creating a table for client demand
-CREATE Table clientdemand (
-	clientdemandid serial not null primary key,
-	clientid int not null,
-	curriculumid int not null,
-	needby date not null,
-	quantitydemanded int not null,
-	constraint clientdemand_client_fk1 foreign key (clientid) references client(clientid),
-	constraint clientdemand_curriculum_fk1 foreign key (curriculumid) references curriculum(curriculumid)
-	);
-
---//////////////////////////////////////////////////////////////////////////
--- creating join tables
---//////////////////////////////////////////////////////////////////////////
-
-DROP TABLE curriculum_skills CASCADE;
-
--- creating a table for curriculum_skills
-CREATE Table curriculum_skills (
-	skillid int NOT NULL,
-	curriculumid int NOT NULL,
-	CONSTRAINT skill_fk FOREIGN KEY (skillid) REFERENCES skills(skillid),
-	CONSTRAINT curriculum_fk FOREIGN KEY (curriculumid) REFERENCES curriculum(curriculumid)
-);
-
-DROP TABLE trainer_skills CASCADE;
-
--- creating a table for trainer_skills
-CREATE Table trainer_skills (
-	trainerid int not null,
-	skillid int not null,
-	CONSTRAINT trainer_fk FOREIGN KEY (trainerid) REFERENCES trainer(trainerid),
-	CONSTRAINT skills_fk FOREIGN KEY (skillid) REFERENCES skills(skillid)
-);
-
-DROP TABLE trainer_curriculum CASCADE;
-
--- creating a table for trainer_curriculum
-CREATE Table trainer_curriculum (
-	trainerid int not null,
-	curriculumid int NOT NULL,
-	CONSTRAINT trainer_fk FOREIGN KEY (trainerid) REFERENCES trainer(trainerid),
-	CONSTRAINT curriculum_fk FOREIGN KEY (curriculumid) REFERENCES curriculum(curriculumid)
-);
-
---**************************************************************************
---**************************************************************************
--- project 3 data to be inserted
---**************************************************************************r
---**************************************************************************
-
--- Example insert 
--- INSERT INTO customer_information (first_name, last_name, member_id) VALUES('Smith', 'James','esokullu@hotmail.com');
 
 --//////////////////////////////////////////////////////////////////////////
 -- creating regular table data
@@ -124,11 +11,11 @@ CREATE Table trainer_curriculum (
 
 
 -- creating table skills
-INSERT INTO skills(skillid, skillname) values(357, 'React-Native');
-INSERT INTO skills(skillid, skillname) values(246, 'Java-SpringBoot');
-INSERT INTO skills(skillid, skillname) values(975, 'Python-DevOps');
-INSERT INTO skills(skillid, skillname) values(405, 'Angular-AWS');
-INSERT INTO skills(skillid, skillname) values(111, 'R-MATLAB');
+INSERT INTO skill(skillid, skillname) values(357, 'React-Native');
+INSERT INTO skill(skillid, skillname) values(246, 'Java-SpringBoot');
+INSERT INTO skill(skillid, skillname) values(975, 'Python-DevOps');
+INSERT INTO skill(skillid, skillname) values(405, 'Angular-AWS');
+INSERT INTO skill(skillid, skillname) values(111, 'R-MATLAB');
 
 
 -- creating a table for curriculum
@@ -181,18 +68,18 @@ INSERT INTO clientdemand (clientid, clientdemandid, curriculumid, needby, quanti
 --//////////////////////////////////////////////////////////////////////////
 
 -- create data for curriculum_skills
-INSERT INTO curriculum_skills(curriculumid, skillid) values(1, 357);
-INSERT INTO curriculum_skills(curriculumid, skillid) values(2, 246);
-INSERT INTO curriculum_skills(curriculumid, skillid) values(3, 975);
-INSERT INTO curriculum_skills(curriculumid, skillid) values(4, 405);
-INSERT INTO curriculum_skills(curriculumid, skillid) values(5, 111);
+INSERT INTO curriculum_skill(curriculumid, skillid) values(1, 357);
+INSERT INTO curriculum_skill(curriculumid, skillid) values(2, 246);
+INSERT INTO curriculum_skill(curriculumid, skillid) values(3, 975);
+INSERT INTO curriculum_skill(curriculumid, skillid) values(4, 405);
+INSERT INTO curriculum_skill(curriculumid, skillid) values(5, 111);
 
      
 -- create data for trainer_skill
-INSERT INTO trainer_skills (trainerid, skillid) VALUES (123, 357);
-INSERT INTO trainer_skills (trainerid, skillid) VALUES (124, 246);
-INSERT INTO trainer_skills (trainerid, skillid) VALUES (125, 975);
-INSERT INTO trainer_skills (trainerid, skillid) VALUES (126, 405);
+INSERT INTO trainer_skill (trainerid, skillid) VALUES (123, 357);
+INSERT INTO trainer_skill (trainerid, skillid) VALUES (124, 246);
+INSERT INTO trainer_skill (trainerid, skillid) VALUES (125, 975);
+INSERT INTO trainer_skill (trainerid, skillid) VALUES (126, 405);
 
 -- create data for trainer_curriculum
 INSERT INTO trainer_curriculum(trainerid, curriculumid) values(123, 1);
@@ -201,9 +88,3 @@ INSERT INTO trainer_curriculum(trainerid, curriculumid) values(125, 3);
 INSERT INTO trainer_curriculum(trainerid, curriculumid) values(126, 4);
 INSERT INTO trainer_curriculum(trainerid, curriculumid) values(127, 5);
 INSERT INTO trainer_curriculum(trainerid, curriculumid) values(128, 6);
-
-
-
-
-
-
---
+++ SQL Scripts/Project 3 Backend Drop Tables.sql
@@ -0,0 +1,28 @@
+--**************************************************************************
+--**************************************************************************
+-- Project 3 Drop Table
+--**************************************************************************
+--**************************************************************************
+
+
+--//////////////////////////////////////////////////////////////////////////
+-- creating regular tables
+--//////////////////////////////////////////////////////////////////////////
+
+DROP TABLE skills CASCADE;
+
+DROP TABLE curriculum CASCADE;
+
+DROP TABLE trainer CASCADE;
+
+DROP TABLE client CASCADE;
+
+DROP TABLE batch CASCADE;
+
+DROP TABLE clientdemand CASCADE;
+
+DROP TABLE curriculum_skills CASCADE;
+
+DROP TABLE trainer_skills CASCADE;
+
+DROP TABLE trainer_curriculum CASCADE;
