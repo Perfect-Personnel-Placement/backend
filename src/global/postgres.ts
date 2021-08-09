80ebb906ca0ea358bc2b9de59af0b421574ae677
Jared Burkamper <jaredburkamper@gmail.com>
2021-08-09 16:28:57 -0500

    Minor lambda and SAM changes

--- src/global/postgres.ts
+++ src/global/postgres.ts
@@ -4,7 +4,6 @@ const client = new Client({
     host: process.env.RDS_URL, 
     user: process.env.RDS_USER, 
     password: process.env.RDS_PASSWORD,
-    connectionTimeoutMillis: 2000,
     database: "P3_Database"
 })
 
