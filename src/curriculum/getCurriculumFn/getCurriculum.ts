11a4a549d83f31db63298735529c0504cb2feec2
Tyler Yates <tyates907@gmail.com>
2021-08-09 13:39:57 -0700

    return join table info when GET requesting by id

--- src/curriculum/getCurriculumFn/getCurriculum.ts
+++ src/curriculum/getCurriculumFn/getCurriculum.ts
@@ -2,6 +2,7 @@ import { APIGatewayProxyEvent } from 'aws-lambda';
 import { HTTPResponse } from '../../global/objects';
 import client from '../../global/postgres';
 const text = 'SELECT * FROM curriculum WHERE curriculumid = $1';
+const skillQuery = 'SELECT * FROM curriculum-skills WHERE curriculumid = $1'
 
 // Written by MJS
 export default async function handler(event: APIGatewayProxyEvent) {
@@ -10,10 +11,12 @@ export default async function handler(event: APIGatewayProxyEvent) {
     return new HTTPResponse(400, 'No path parameter was given');
   }
   const currId = event.pathParameters.curriculumId;
-
+  
   // Return data or error from database
   try {
     const res = await client.query(text, [currId]);
+    const skillArray = await client.query(skillQuery, [currId]);
+    res.rows[0].skills = skillArray.rows;
     return new HTTPResponse(200, res.rows);
   } catch (err) {
     return new HTTPResponse(400, {
--- src/trainer/getTrainerByIdFn/getTrainerById.ts
+++ src/trainer/getTrainerByIdFn/getTrainerById.ts
@@ -2,6 +2,7 @@ import { APIGatewayProxyEvent } from 'aws-lambda';
 import { HTTPResponse } from '../../global/objects';
 import client from '../../global/postgres';
 const text = 'SELECT * FROM trainer WHERE (trainerid = $1) RETURNING *';
+const curriculumQuery = 'SELECT * FROM trainer-curriculum WHERE trainerid = $1'
 
 //Comments written by Samuel Smetzer
 export default async function handler(event: APIGatewayProxyEvent) {
@@ -16,11 +17,12 @@ export default async function handler(event: APIGatewayProxyEvent) {
     //Try querying the DataBase
     try {
         res = await client.query(text, trainerData)
-
+        const curriculumArray = await client.query(curriculumQuery, [trainer])
+        res.rows[0].curricula = curriculumArray.rows
     } catch (err) {
         console.log(err);
         return new HTTPResponse(400, "Unable to Query the information")
     }
-    //Return the row deleted
+    //Return the row
     return new HTTPResponse(200, res.rows)
 };
