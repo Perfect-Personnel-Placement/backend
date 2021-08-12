efa4d9e8681413f5b07f98f05d26344c0d2f7fe5
marcski55 <marc.skwarczynski@revature.net>
2021-08-12 13:33:51 -0700

    Edited GetAllDemand

--- src/demand/getAllDemandsFn/getAllDemands.ts
+++ src/demand/getAllDemandsFn/getAllDemands.ts
@@ -1,8 +1,8 @@
-import { HTTPResponse } from "../../global/objects";
-import { APIGatewayProxyEvent } from "aws-lambda";
+import { HTTPResponse } from '../../global/objects';
+import { APIGatewayProxyEvent } from 'aws-lambda';
 
-import client from "../../global/postgres";
-const text = "SELECT * FROM demand";
+import client from '../../global/postgres';
+const text = 'SELECT * FROM demand';
 
 /**
  * Get All Demands Handler - Used to get all demands from the database.
@@ -10,7 +10,7 @@ const text = "SELECT * FROM demand";
  * @returns {HTTPResponse} - HTTP response with status code and body
  * @author Daguinson Fleurantin, Nick Wang
  */
-export default async function handler(event: APIGatewayProxyEvent) {
+export default async function handler() {
   // Assign data or return error provided by the database
   let res;
   try {
@@ -22,11 +22,11 @@ export default async function handler(event: APIGatewayProxyEvent) {
     if (err.detail) {
       displayError = err.detail;
     } else {
-      displayError = "Unknown error.";
+      displayError = 'Unknown error.';
     }
     return new HTTPResponse(400, {
-      error: "The database has rejected the query.",
-      db_error: displayError,
+      error: 'The database has rejected the query.',
+      db_error: displayError
     });
   }
 }
