d91d197b65539f18d10430ed2350e944490ee8dc
Jared Burkamper <jaredburkamper@gmail.com>
2021-08-12 16:21:14 -0500

    Init endpoint changes, SQL structure changes, Template update, installed ESLint

--- src/init/init.test.ts
+++ src/init/init.test.ts
@@ -1,44 +1,53 @@
 jest.mock("../global/postgres", () => {
-    return {
-        connect: jest.fn(),
-        query: jest.fn()
-    }
+  return {
+    connect: jest.fn(),
+    query: jest.fn(),
+  };
 });
 jest.mock("fs", () => {
-    return {
-        readFileSync: jest.fn()
-    }
+  return {
+    readFileSync: jest.fn(),
+  };
 });
 
+import { APIGatewayProxyEvent } from "aws-lambda";
 import fs from "fs";
 import pgClient from "../global/postgres";
 import handler from "./init";
 
 describe("Function handler for init database function", () => {
-    it("Should run successfully without errors", async () => {
-        (fs.readFileSync as jest.Mock).mockImplementationOnce(() => {
-            return {
-                toString: () => {return "bigfoot"}
-            }
-        });
-        const response = await handler();
-        expect(response.statusCode).toEqual(200);
+  it("Should run successfully without errors", async () => {
+    (fs.readFileSync as jest.Mock).mockImplementationOnce(() => {
+      return {
+        toString: () => {
+          return "bigfoot";
+        },
+      };
     });
+    const response = await handler({} as APIGatewayProxyEvent);
+    expect(response.statusCode).toEqual(200);
+  });
 
-    it("Should fail gracefully if FS fails", async () => {
-        (fs.readFileSync as jest.Mock).mockImplementationOnce(() => {throw "error"});
-        const response = await handler();
-        expect(response.statusCode).toEqual(500);
+  it("Should fail gracefully if FS fails", async () => {
+    (fs.readFileSync as jest.Mock).mockImplementationOnce(() => {
+      throw "error";
     });
+    const response = await handler({} as APIGatewayProxyEvent);
+    expect(response.statusCode).toEqual(500);
+  });
 
-    it("Should fail gracefully if PG fails", async () => {
-        (pgClient.query as jest.Mock).mockImplementationOnce(() => {throw "error"});
-        (fs.readFileSync as jest.Mock).mockImplementationOnce(() => {
-            return {
-                toString: () => {return "bigfoot"}
-            }
-        });
-        const response = await handler();
-        expect(response.statusCode).toEqual(500);
+  it("Should fail gracefully if PG fails", async () => {
+    (pgClient.query as jest.Mock).mockImplementationOnce(() => {
+      throw "error";
     });
-})
+    (fs.readFileSync as jest.Mock).mockImplementationOnce(() => {
+      return {
+        toString: () => {
+          return "bigfoot";
+        },
+      };
+    });
+    const response = await handler({} as APIGatewayProxyEvent);
+    expect(response.statusCode).toEqual(500);
+  });
+});
