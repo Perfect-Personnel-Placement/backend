2dd029fe159e10b89ed3935b8b899d9748191eae
mat2718 <matthewterry68.mt@gmail.com>
2021-08-13 10:34:23 -0400

    Update jest.config.ts

--- jest.config.ts
+++ jest.config.ts
@@ -25,7 +25,7 @@ export default {
   // collectCoverageFrom: ["src/**/*.ts"],
   collectCoverageFrom: ["<rootDir>/src/**/*.ts"],
 
-  // The directory where Jest should output its coverage files
+  // The directory where Jest should output its coverage files 
   coverageDirectory: "coverage",
 
   // An array of regexp pattern strings used to skip coverage collection
